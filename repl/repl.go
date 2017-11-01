707c7e26eac6482bd4abce269c66ca5410470724
Jason Messer <jasoncmesser@gmail.com>
2017-10-31 21:16:41 -0700

    add basic repl

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"theduke/lexer"
+	"theduke/token"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const (
+	PROMPT		= ">> "
+)
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
