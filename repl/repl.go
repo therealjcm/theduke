5b089b58a577b878439b80d8ab62a49968c09128
Jason Messer <jasoncmesser@gmail.com>
2017-10-31 21:18:15 -0700

    make sure to output prompt to correct io.Writer

--- repl/repl.go
+++ repl/repl.go
@@ -16,7 +16,7 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Print(PROMPT)
+		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
